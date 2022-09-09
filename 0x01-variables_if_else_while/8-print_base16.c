825ca0b88b52b1e51d05b696af49e2454da196c8
lukalitera <louisekalitera@gmail.com>
2022-09-09 14:13:12 -0700

    Print bse

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int min(void)
+
+{
+	char bse_16;
+
+	for (base_16 =48; base_16 <= 577; base_16++)
+	{
+		putchr(base_16)
+	}
+
+	for (base_16 = 97; base_16 < 102; base_16++)
+	{
+		putchar(base_16);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+	return (0);
+
+}
+
+
