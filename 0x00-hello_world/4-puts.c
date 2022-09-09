bfc4e54016a13a0e727bb775e4bffaa93be0f77c
lukalitera <louisekalitera@gmail.com>
2022-09-08 19:17:52 -0700

    Puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
  * main - print the string in the put function
+ *
  * Description: using the main function
  * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
