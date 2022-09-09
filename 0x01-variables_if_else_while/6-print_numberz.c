208470fdc06a3eb5ff81d4a92623739c9c7cc8f1
lukalitera <louisekalitera@gmail.com>
2022-09-09 07:11:11 -0700

    print numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar (i);
+	}
+
+	putchar ('\n');
+
+	returtn (0);
+
+}
