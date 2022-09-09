34e874a48d157610dd35c89b0745cc6b60e7991e
lukalitera <louisekalitera@gmail.com>
2022-09-09 07:19:27 -0700

    print tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#includ <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Correct)
+ */
+int main(void)
+
+{
+	char alphabet;
+
+	for (alphabet = 122; alphabet >= 97; alphabet--)
+
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
