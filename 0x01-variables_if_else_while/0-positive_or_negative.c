b91b3a0afb86175bc60de871405982d6ebac1c4e
lukalitera <louisekalitera@gmail.com>
2022-09-10 02:11:33 -0700

    Positive or negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,7 +13,7 @@ int main(void)
 	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
+	
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -26,6 +26,5 @@ int main(void)
 	{
 		printf("%i is zero\n", n);
 	}
-
 	return (0);
 }
