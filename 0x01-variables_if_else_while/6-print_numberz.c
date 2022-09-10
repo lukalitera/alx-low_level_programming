9347b8b5304b1181e2e009eb543569d61218136d
lukalitera <louisekalitera@gmail.com>
2022-09-10 02:42:46 -0700

    Print numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,21 +1,18 @@
-#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success/correct)
  */
 int main(void)
-
 {
 	int i;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar (i);
 	}
-
 	putchar ('\n');
-
 	returtn (0);
-
 }
