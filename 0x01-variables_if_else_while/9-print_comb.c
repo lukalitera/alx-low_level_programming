a830172a54d3d94e329267d315ecc234b3aab8ce
lukalitera <louisekalitera@gmail.com>
2022-09-09 19:30:01 -0700

    Print comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 Success
+ */
+int main(void)
+
+{
+	for (number = 48 <= 57; number++)
+	{
+		putchar(number);
+		if (number == 57)
+	{
+		break;
+	}
+		putchar(',');
+		putchar(' ')
+	}
+	
+	putchar(',');
+	
+	return (0);
+
+}
