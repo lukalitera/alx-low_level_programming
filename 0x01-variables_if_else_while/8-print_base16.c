61903355ddcfe441dc138ef05d351d9eadae4054
lukalitera <louisekalitera@gmail.com>
2022-09-10 03:00:04 -0700

    Print base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,30 +1,25 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Return: 0 (Success)
+ * Return:  Always 0 (Success)
  */
 int min(void)
-
 {
-	char bse_16;
+	int i;
+	char j;
 
-	for (base_16 =48; base_16 <= 577; base_16++)
+	for (i = 48; i < 58; i++)
 	{
-		putchr(base_16)
+
+		putchar(i);
 	}
 
-	for (base_16 = 97; base_16 < 102; base_16++)
+	for (j = 'a'; i <= 'f'; j++)
 	{
-		putchar(base_16);
+		putchar(j);
 	}
-
 	putchar('\n');
-
 	return (0);
-
-	return (0);
-
 }
-
-
