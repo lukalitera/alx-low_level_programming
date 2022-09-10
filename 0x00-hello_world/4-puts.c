d87467d7de6cfeaba5ffb4a990141ba927b869cb
lukalitera <louisekalitera@gmail.com>
2022-09-09 20:42:03 -0700

    Puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * Description: main - Prints out a string to stdout.
- *  Return: 0 if success.
+ * main - entry point
+ *
+ * Return: 0 after printing the function
  */
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
-Return: 0
+
+Return: (0);
 
 }
