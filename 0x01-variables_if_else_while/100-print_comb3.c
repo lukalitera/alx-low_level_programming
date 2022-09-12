1681babb25ed885e892ed6f1ec51cce19e2cfba9
lukalitera <louisekalitera@gmail.com>
2022-09-12 06:50:27 -0700

    Print comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
  * main - entry point
  *
- * codes for printing all possible combintion of two d/t digits
- * in assending order nd separated by a comma folled by space
- * 
- * Return: 0 Success
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int digit1, digit 2;
 
-	for (digit1 = 0; digit1 <9; digit1++)
+	int d;
+
+	for (d = 0; d <9; d++)
 	{
-		for (digit2 = digit1 + 1; digig2 < 10; digit2++)
+		putchar((digit1 % 10) + '0');
+		putchar((digit2 % 10) + '0');
+		if (d != 99)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-
-			if (digit1 == 8 && digit2 == 9)
-				continue;
-
 			putchar(',');
 			putchar(' ');
 		}
