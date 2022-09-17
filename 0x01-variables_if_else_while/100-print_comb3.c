c93608dc55b0bf1d80a8995b9d228e0a96303e66
lukalitera <louisekalitera@gmail.com>
2022-09-17 05:46:02 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,8 +10,8 @@
 
 int main(void)
 {
-	int i,j;
-	
+	int i, j;
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 1; j <= 9; j++)
@@ -21,7 +21,7 @@ int main(void)
 				putchar(i + '0');
 				putchar(j + '0');
 
-				if (i !=8)
+				if (i != 8)
 
 				{
 					putchar(',');
