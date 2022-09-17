05907e7b2f8db3f875ceaa50b488675878c92cb1
lukalitera <louisekalitera@gmail.com>
2022-09-17 02:51:43 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
- * Description: main block
- * Return: 0 (success)
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * Return: 0 if success
  */
+
 int main(void)
 {
-
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf('with proper grammar, but the outcome is a piece of art,\n');
 	return (0);
 }
