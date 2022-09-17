8f1b02e48a7fe8adc2a4a85f73376e04a8f39b1f
lukalitera <louisekalitera@gmail.com>
2022-09-17 02:55:15 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
- *
- * Description: using the main function
+ * main - print out a string to stdout
  * Return: 0 if success
  */
 
