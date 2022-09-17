c06d0551d11be4ed0c824651a0ae9f83fa37d8f7
lukalitera <louisekalitera@gmail.com>
2022-09-16 20:13:33 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ * @n: parameter
+ * Return: returns nothing
+ */
+
+void print_lines(int n)
+{
+	while (n-- > 0)
+	{
+		putchar('_');
+	}
+	putchar('\n');
+}
