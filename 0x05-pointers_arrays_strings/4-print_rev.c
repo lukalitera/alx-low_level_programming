2e61046f41a1fe0781909eec716051649135683d
lukalitera <louisekalitera@gmail.com>
2022-09-19 18:42:43 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: A pointer to an int that will be changed
+ * 
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0');
+	{
+		i**;
+	}
+}
