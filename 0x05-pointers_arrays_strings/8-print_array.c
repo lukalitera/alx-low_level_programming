79eecf76d33e94b3de6bc084e80265a5542811b8
lukalitera <louisekalitera@gmail.com>
2022-09-20 05:32:54 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,17 +11,14 @@
 
 void print_array(int *a, int n)
 {
-	int i;
+	int inc;
 
-	for (i = 0; i < n; i++)
+	for (inc = 0; inc < n; inc++)
 	{
-		printf("%d", a[i]);
-
-		if (i != n - 1)
-		{
-			printf(",");
-			print(" ");
-		}
-
-		printf("\n");
+		if (inc != n - 1)
+			printf("%d, ", a[inc]);
+		else
+			printf("%d", a[inc]);
+	}
+	putchar(10);
 }
