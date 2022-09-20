cbb410175288c9cd2e080f068b34e0a0ec9df992
lukalitera <louisekalitera@gmail.com>
2022-09-19 18:35:34 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,2 +1,14 @@
 #include "main.h"
 
+/**
+ * _puts - prints a string
+ * @str: pointer to an int that will be updated
+ */
+void _puts(char *str)
+{
+	for (; *srt != '\0'; str++)
+	{
+		_putchar(*str)
+	}
+}
+	
