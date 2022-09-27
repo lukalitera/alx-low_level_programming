545db4817a8e84d6185d3ac75f1a9b5fde157c9b
lukalitera <louisekalitera@gmail.com>
2022-09-27 05:03:54 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,10 +12,16 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; i < 8; i++)
+	i = 0;
+	while (i < 8)
 	{
-		for (j = 0; j < 8; j++)
+		j = 0;
+		while (j < 8)
+		{
 			_putchar(a[i][j]);
-		_putchar('\n');
+			j++;
+		}
+		_putchar ('\n');
+		i++;
 	}
 }
