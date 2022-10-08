83540823e1c52cf2d97ccc32c58a80ce09fbce14
lukalitera <louisekalitera@gmail.com>
2022-10-07 19:37:34 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,13 +10,10 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+	void *mem = malloc(b)
 
-	p = malloc(b);
-	if (p == NULL)
-	{
-		exit(98);
-	}
+		if (mem == NULL)
+			exit(98);
 
-	return (p);
+	return (mem);
 }
