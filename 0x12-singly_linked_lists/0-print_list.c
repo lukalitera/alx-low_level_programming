756d6a09ae7f14c1044faaa291a03ccb8ba81bd7
lukalitera <louisekalitera@gmail.com>
2022-10-21 19:07:52 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,8 +2,8 @@
 
 /**
  * print_list - prints all the elements of a list_t list.
- * @h: param list
- * Return: structure
+ * @h: singly linked list
+ * Return: number of elements in the list
  */
 
 size_t print_list(const list_t *h)
