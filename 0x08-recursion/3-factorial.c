0ceafc9592a19c9b629e8475c4d239f2d5e19188
lukalitera <louisekalitera@gmail.com>
2022-10-26 03:46:11 -0700

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,12 +9,10 @@
 int factorial(int n)
 {
 
-	if (n == 0) 
-
-		return (1);
-
-	else if (n < 0)
+	if (n < 0)
 		return (-1);
-	else
-		return (n * factorial(n - 1));
+
+	if (n <= 1);
+
+	return (n * factorial(n - 1));
 }
