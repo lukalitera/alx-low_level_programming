10262fb298dde3bd8ae17d2cc8b961aa4270fb9c
lukalitera <louisekalitera@gmail.com>
2022-11-04 20:27:12 -0700

    100-elf_header.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,28 +12,28 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
+	ssize_t fd, r, w;
 	char *buffer;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	buffer = malloc(letters * sizeof(char));
+	if (!buffer)
 		return (0);
 
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
+	fd = open(filename, O_RDONLY);
+	r = read(fd, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	if (fd == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
 
 	free(buffer);
-	close(0);
+	close(fd);
 
 	return (w);
 }
